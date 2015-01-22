0c4e9fe3eba6af0bb28af034345489bea8d6c2ad
Mathurin Dorel <mathurin.dorel@curie.fr>
2015-01-22 16:10:55 +0100

    Function to save the data in local files

--- R/fetch_cBio.R
+++ R/fetch_cBio.R
@@ -1,6 +1,5 @@
 library(cgdsr)
-library(libCurl)
-#library(RNaviCell)
+library(RCurl)
 
 # Gene lists of the ACSN maps (.owl for the map, .gmt for the gene list)
 ACSN_genes = list("global"="http://acsn.curie.fr/files/acsn_v1.0", "survival"="http://acsn.curie.fr/files/survival_v1.0", "apoptosis"="http://acsn.curie.fr/files/apoptosis_v1.0", "cell_motility"="http://acsn.curie.fr/files/emtcellmotility_v1.0", "cell_cycle"="http://acsn.curie.fr/files/cellcycle_v1.0", "DNA_repair"="http://acsn.curie.fr/files/dnarepair_v1.0")
@@ -25,7 +24,7 @@ getGenesList <- function(url="http://acsn.curie.fr/files/acsn_v1.0.gmt"){
 #' @return A list of data.frames containing the data for each gene, for all combination experiment x sample
 #' @export
 #' @seealso \code{\link{importDataSet}}, \code{\link{saveDataSet}}
-#' @author Mathurin Dorel \email{mathurin.dorel@curie.fr}
+#' @author Mathurin Dorel \email{mathurin.dorel@@curie.fr}
 # TODO replace by the url of the map
 cBioDataSet <- function (conn, profile_id, case_id, genes_url="http://acsn.curie.fr/files/acsn_v1.0.gmt") {
     genes_data = list()
@@ -68,19 +67,4 @@ saveDataSet <- function(fname, dataset) {
 importDataSet  <- function(fname) {
     fcontent = readLines(fname)
 }
-
-conn = CGDS("http://www.cbioportal.org/")
-
-# Selection of the study id
-studies = getCancerStudies(conn)
-st_id = studies$cancer_study_id[1]
-
-# Select all patients
-#cases = getCaseLists(conn, st_id) # Use and display information ?
-ca_id = paste0(st_id, "_all")
-# Retrieve genetic profiles ids of all profiles
-profiles = getGeneticProfiles(conn, st_id)
-pr_id = profiles$genetic_profile_id
-
-genes_data = retrieveDataSet(conn, pr_id, ca_id)
 
--- R/genes_data.R
+++ R/genes_data.R
@@ -1,7 +1,6 @@
-# S4 class for genes data
-# Contains statistics and functions to communicate with a NaviCell API
 
-library(method)
+library(methods)
+#library(RNaviCell)
 
 check_dataset <- function(object) {
     errors = c() 
@@ -27,6 +26,8 @@ check_dataset <- function(object) {
 }
 
 NCclass = "NCviz"
+#' S4 class for genes data
+#' Contains statistics and functions to communicate with a NaviCell API
 setClass(NCclass,
      representation(
             nc_url="character",
@@ -39,6 +40,14 @@ setClass(NCclass,
      validity=check_dataset
      )
 
+#' Constructor for the class NCviz, which provides functions to visualize profiling data on a NaviCell map
+#' @param nc_url URL of the NaviCell server
+#' @param cell_type Name of the dataset to pass to the NaviCell server
+#' @param cbio_data Data loaded with cBioStudy (incompatible with nc_data)
+#' @param nc_data Data under the NaviCell format, a list indexed by experiment type containing data arranged in a dataframe of dimensions genes * samples (incompatible with cbio_data)
+#' @export
+#' @seealso cBioStudy
+#' @author Mathurin Dorel \email{mathurin.dorel@@curie.fr}
 NCviz <- function(nc_url="", cell_type="", cbio_data=list(), nc_data=list(), verbose=TRUE) {
     return(new( NCclass, nc_url=nc_url, cell_type=cell_type, cbio_data=cbio_data, nc_data=nc_data, verbose=verbose ))
 }
@@ -70,7 +79,7 @@ setMethod("initialize",
                     }
                 } else {
                     .Object@cbio_data[[length(rownames(.Object@nc_data[[1]]))]] = 0
-                    print("No conversion from navicell format to cbioportal format, as it is useless")
+                    print("No conversion from navicell format to cbioportal format") # as it is useless
                     for (gene in rownames(.Object@nc_data[[1]])) {
                         # TODO ? copy in not navicell format
                     }
@@ -81,8 +90,8 @@ setMethod("initialize",
           )
 
 
-setGeneric(addData) # Add data tables
-setGeneric()
+#setGeneric(addData) # Add data tables
+#setGeneric()
 
 display_function="NCdisplay" # Display the data in a good manner in NaviCell
 setGeneric(display_function, function(obj){return(standardGeneric(display_function))})
@@ -91,4 +100,20 @@ setMethod(display_function, NCclass,
             # TODO display in NaviCell
             }
          )
+
+#' Save the data in several files, one file per profiling method. Each file can then be imported into NaviCell.
+saveInFiles <- function(obj) {
+    for (method in names(obj@nc_data)) {
+        print(paste("Saving", method))
+        ff = open(paste0(gsub(" ", "_", obj@cell_type), "_", method, ".tsv"), "w")
+        writeLines(c("GENE", colnames(obj@nc_data[[method]])), sep="\t")
+
+        for (gene in rownames(obj@nc_data[[method]])) {
+            writeLines(c(gene, obj@nc_data[[method]][gene,]), sep="\t")
+        }
+        close(ff)
+    }
+}
+setGeneric("saveInFiles", saveInFiles)
+setMethod("saveInFiles", NCclass, saveInFiles)
 
