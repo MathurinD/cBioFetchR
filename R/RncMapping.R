8cfdec80eb02f4b6bacf5a0c1cf2bc29d0ef8575
Mathurin Dorel <mathurin.dorel@curie.fr>
2015-01-23 10:41:03 +0100

    Added documentation files

---
+++ R/RncMapping.R
@@ -0,0 +1,21 @@
+#' Visualize profiling data on a NaviCell map
+#'
+#' Provides functions to visualize any kind of biological profiling data on a NaviCell map in a biologically relevant manner.
+#' Also contains functions to import datasets from cBioportal which format them for NaviCell export.
+#'
+#' @docType package
+#' @name RncMapping-package
+#' @author Mathurin Dorel \email{mathurin.dorel@@curie.fr}
+#' @author Maintainer : Mathurin Dorel \email{mathurin.dorel@@curie.fr}
+#' @keywords manip
+#' @references TODO
+#' @examples
+#' conn = CGDS("http://www.cbioportal.org/")
+#'
+#' #Selection of the study id
+#' studies = getCancerStudies(conn)
+#' st_id = studies$cancer_study_id[1]
+#'
+#' genes_data = cBioStudy(st_id)
+#' visualizer = NCviz(nc_url="http://acsn.curie.fr/files/acsn_v1.0.owl", cell_type="Acute Myeloid Leukemia", cbio_data=genes_data)
+NULL
