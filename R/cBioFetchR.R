be4ca325d75b4881055f51beb2ae870d4a8a5859
Mathurin Dorel <mathurin.dorel@curie.fr>
2015-06-09 12:10:17 +0200

    Rename package to cBioFetchR

--- R/cBioFetchR.R
+++ R/cBioFetchR.R
@@ -4,7 +4,7 @@
 #' Also contains functions to import datasets from cBioportal which format them for NaviCell export.
 #'
 #' @docType package
-#' @name RncMapping-package
+#' @name cBioFetchR-package
 #' @author Mathurin Dorel \email{mathurin.dorel@@curie.fr}
 #' @author Maintainer : Mathurin Dorel \email{mathurin.dorel@@curie.fr}
 #' @keywords manip
@@ -15,7 +15,9 @@
 #' #Selection of the study id
 #' studies = getCancerStudies(conn)
 #' st_id = studies$cancer_study_id[1]
-#'
-#' genes_data = cBioStudy(st_id)
-#' visualizer = NCviz(nc_url="http://acsn.curie.fr/files/acsn_v1.0.owl", cell_type="Acute Myeloid Leukemia", cbio_data=genes_data)
+#' 
+#' # Fetch data from cBioPortal for ACSN genes
+#' fetcher= cBioNCviz(genes_list="http://acsn.curie.fr/files/acsn_v1.1.gmt", cell_type="Acute Myeloid Leukemia", cbio_data=genes_data)
+#' # Save Data
+#' saveDat(fetcher)
 NULL
